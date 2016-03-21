8f40e46eca953b1c6c97efaa20157ef4d0f1c777
Ghithub <gnewsgroup@gmail.com>
2016-03-21 16:17:15 -0400

    Added ngRoute for routing.

--- MvcAngular/Scripts/MvcAngular.js
+++ MvcAngular/Scripts/MvcAngular.js
@@ -1,2 +1,16 @@
-﻿var MvcAngular = angular.module("MvcAngular", []);
+﻿var MvcAngular = angular.module("MvcAngular", ["ngRoute"]);
 MvcAngular.controller("LandingPageController", LandingPageController);
+var configFunction = function ($routeProvider) {
+    $routeProvider.
+       when("/routeOne", {
+           templateUrl: "routesDemo/one"
+       })
+        .when("/routeTwo", {
+            templateUrl: "routesDemo/two"
+        })
+        .when("/routeThree", {
+            templateUrl: "routesDemo/three"
+        });
+}
+configFunction.$inject = ["$routeProvider"];
+MvcAngular.config(configFunction);
